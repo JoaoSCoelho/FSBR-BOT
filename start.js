61b80d2b09c11bfead5acced0006214e43abe5e9
João Victor <joaovictor-san-co@hotmail.com>
2021-11-20 08:30:41 -0300

    Update start.js

--- start.js
+++ start.js
@@ -1,1 +1,11 @@
-require('./dist')
+const cp = require('child_process')
+
+cp.exec('yarn tsc', (err, _stdout, stderr) => {
+  if (err) {
+    console.error(err)
+  } else if (stderr) {
+    console.error(stderr)
+  } else {
+    require('./dist')
+  }
+})
